0a28d5ceaf4e8ab2f74713e5f5261a8273b5268f
Rahman-Mostafizur <ms.mr@outlook.com>
2019-12-11 16:55:27 +0600

    Add project files.

---
+++ DemoCoreApp/Models/Contact.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoCoreApp.Models
+{
+    public class Contact
+    {
+        private int id;
+        [Key]
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [Phone]
+        [Required]
+        public string Phone { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+
+    }
+
+    public class ContactContext:DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        public ContactContext(DbContextOptions<ContactContext> opt):base(opt)
+        {
+
+        }
+    }
+
+}
